4330e0aa0c5c268ecc5900302baa3793d7de0cfb
budi-irawan <budi.irawan171717@gmail.com>
2022-10-24 19:23:02 +0700

    landing

---
+++ module/users/model.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const {sq} =  require('../../config/connection');
+
+const users = sq.define('users',{
+    id:{
+        type: DataTypes.UUID,
+        primaryKey: true,
+    },
+    username:{
+        type:DataTypes.STRING
+    },
+    password:{
+        type:DataTypes.STRING
+    },
+    role:{
+        type:DataTypes.STRING
+    }
+},
+{
+paranoid:true,
+freezeTableName:true
+});
+
+module.exports = users
